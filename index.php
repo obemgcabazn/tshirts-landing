904540a672b674c3ae93e76c28cb56cb0e14d0df
Spaceweb <office@spaceweb.studio>
2018-02-21 17:12:41 +0300

    upd

--- index.php
+++ index.php
@@ -39,25 +39,25 @@
       <section id="first-screen">
 
         <div class="row">
-          <div class="col-12 col-md-5">
-            <h1>Заказать футболку<br><span class="font-weight-bold"><?=$title_text ?></span></h1>
-            <form>
+          <div class="col-12 col-md-6 col-lg-5">
+            <h1 class="text-center text-sm-left">Заказать футболку<br><span class="font-weight-bold"><?=$title_text ?></span></h1>
+            <form action="mail.php" method="POST" id="top-form" class="d-none d-lg-block">
               <div class="row">
                 <div class="col-9">
                   <div class="form-group form-input">
                     <label for="inpit-name">Имя</label>
-                    <input type="text" class="form-control" id="input-name" placeholder="Введите имя">
+                    <input type="text" class="form-control" id="input-name" placeholder="Введите имя" required="true">
                   </div>
                   <div class="form-group form-input">
                     <label for="input-phone">Телефон</label>
-                    <input type="text" class="form-control" id="input-phone" placeholder="+7 (">
+                    <input type="text" class="form-control" id="input-phone" placeholder="+7 (" required="true">
                   </div> 
                 </div>
               </div>
               <div class="row">
                 <div class="col-10">
                   <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" checked>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" checked required="true">
                     <label class="form-check-label" for="exampleCheck1">Нажимая кнопку «Отправить» Вы принимаете <a href="">соглашение на обработку персональныз данных</a>.</label>
                   </div>
                   <button type="submit" class="btn btn-red">Отправить</button>
@@ -65,17 +65,12 @@
               </div>
             </form>
           </div>
-          <div class="col">
+          <div class="col col-md-6 col-lg-7">
             <p class="text-right"><img src="img/<?=$title?>.png" class="tshirt <?=$title?>"></p>
             <div class="price">1000 ₽</div>
           </div>
-          <div class="col-12 d-sm-none">
-            <p class="desc"> 
-              Женские и мужские, размеры XS-XXL
-            </p>
-          </div>
-          <div class="col text-center d-sm-none">
-            <button class="btn-red">
+          <div class="col-12 text-center d-lg-none">
+            <button type="button" class="btn btn-red" data-toggle="modal" data-target="#exampleModal">
               Заказать
             </button>
           </div>
@@ -83,41 +78,41 @@
       </section>
     </div>
   </div> <!--wrapper -->
- 
+
   <section id="advantages">
     <div class="container">
       <div class="row">
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/003-washing-machine-for-laundry.svg" alt="" class="advantages-icon">
             <p>Выдерживает <br> <b>50 стирок</b></p>
           </div>
         </div>
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/001-startup.svg" alt="" class="advantages-icon">
             <p>Печать<br><b>в день обращения</b></p>
           </div>
         </div>
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/007-cotton-polo-shirt.svg" alt="" class="advantages-icon">
             <p>Печать <b>на своих</b><br>или <b>Ваших</b> футболках</p>
           </div>
         </div>
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/006-cotton.svg" alt="" class="advantages-icon">
             <p><b>100% хлопок</b></p>
           </div>
         </div>
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/008-soccer-t-shirt.svg" alt="" class="advantages-icon">
             <p>Различные<br> способы печати</p> 
           </div>
         </div>
-        <div class="col col-md-4 text-center">
+        <div class="col-12 col-sm-4 col-md-4 text-center">
           <div class="advantages-item">
             <img src="img/002-package.svg" alt="" class="advantages-icon">
             <p><b>Доставка</b><br>Курьером</p>
@@ -137,15 +132,15 @@
         <div class="row d-flex justify-content-center">
           <form>
             <div class="form-row form-input">
-              <div class="col col-md-4">
+              <div class="col-12 col-sm-4">
                 <label for="input-bottom-name">Имя</label>
                 <input type="text" name="input-bottom-name" id="input-bottom-name" class="form-control" placeholder="Имя">
               </div>
-              <div class="col col-md-4">
+              <div class="col-12 col-sm-4">
                 <label for="input-bottom-phone">Телефон</label>
                 <input type="phone" name="input-bottom-phone" id="input-bottom-phone" class="form-control" placeholder="+7 (">
               </div>
-              <div class="col col-md-4">
+              <div class="col-12 col-sm-4">
                 <button type="submit" class="btn btn-red">Отправить заявку</button>
               </div>
             </div>
@@ -166,9 +161,38 @@
       </div>
     </div>
   </section>
- 
-  <script src="js/jquery-3.2.1.slim.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Отправить заявку</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form>
+          <div class="modal-body">
+             <div class="form-group">
+              <label for="exampleInputEmail2">Имя</label>
+              <input type="text" class="form-control" id="exampleInputName2" placeholder="Имя">
+            </div>
+            <div class="form-group">
+             <label for="exampleInputEmail2">Телефон</label>
+             <input type="phone" class="form-control" id="exampleInputPhone2" placeholder="+7 (">
+           </div>
+         </div>
+         <div class="modal-footer">
+          <button type="button" class="btn btn-red">Отправить</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
